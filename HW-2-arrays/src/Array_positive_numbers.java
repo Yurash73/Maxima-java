e762d2f386d64b2a6db8d8bfeb578080a537ddab
Yury Shevchenko <yury.shevchenko@gmail.com>
2024-06-16 12:10:20 +0500

    HW-2 ver. 1.0.1

--- HW-2-arrays/src/Array_positive_numbers.java
+++ HW-2-arrays/src/Array_positive_numbers.java
@@ -7,13 +7,13 @@ public class Array_positive_numbers {
         int[] array = new int[10];
 
         System.out.println("Нужно ввести 10 целых числе, в конце выведу только положиткльные !");
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < array.length; i++) {
             System.out.print(" Введите " + (i + 1) + "-е число: ");
             array[i] = scanner.nextInt();
         }
 
         System.out.println(" Выводим только положительные числа из массива: ");
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < array.length; i++) {
             if (array[i] >= 0) {
                 System.out.print(array[i] + " ");
             }
